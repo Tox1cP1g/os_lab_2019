ca34aef8bbc603925761f1602523d59b641abb9d
Viktor Kobelev <vak2004@list.ru>
2024-11-08 22:14:14 +0000

    add

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -10,8 +10,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <pthread.h>
-#include "utils.h"
-
 
 struct FactorialArgs {
     uint64_t begin;
@@ -19,17 +17,17 @@ struct FactorialArgs {
     uint64_t mod;
 };
 
-// uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-//     uint64_t result = 0;
-//     a = a % mod;
-//     while (b > 0) {
-//         if (b % 2 == 1)
-//             result = (result + a) % mod;
-//         a = (a * 2) % mod;
-//         b /= 2;
-//     }
-//     return result % mod;
-// }
+uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
+    uint64_t result = 0;
+    a = a % mod;
+    while (b > 0) {
+        if (b % 2 == 1)
+            result = (result + a) % mod;
+        a = (a * 2) % mod;
+        b /= 2;
+    }
+    return result % mod;
+}
 
 uint64_t Factorial(const struct FactorialArgs *args) {
     uint64_t ans = 1;
